722f50129eb27c1b2e932a12572f3688eb6baefe
Gabriella Meggyes <gabriella@meggy.es>
2022-07-28 06:19:21 +0300

    jwt token error fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,69 +1,66 @@
-const User = require('../models/users')
-const Polemove = require('../models/polemoves')
+const User = require("../models/users");
+const Polemove = require("../models/polemoves");
 
-const ObjectId = require('mongodb').ObjectID
-const nodemailer = require('nodemailer')
-const sendgridTransport = require('nodemailer-sendgrid-transport')
+const ObjectId = require("mongodb").ObjectID;
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
-const jwt = require('jsonwebtoken')
-const { jsonSecret } = require('../config/keys')
-const bcrypt = require('bcrypt')
-const { validationResult } = require('express-validator')
-const { response } = require('express')
+const jwt = require("jsonwebtoken");
+const { jsonSecret } = require("../config/keys");
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
+const { response } = require("express");
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
     auth: {
       api_key:
-        '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',
+        "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",
     },
   })
-)
+);
 
 const mail = `<h1>${this.subject}</h1>
   <div style="background-color:black">
   <p>Testing this out</p>
-  </div>`
+  </div>`;
 
 // Auth
 signToken = (user) => {
   return jwt.sign(
     {
-      iss: 'polepath',
+      iss: "polepath",
       sub: user._id,
       iat: new Date().getTime(),
       exp: new Date().setDate(new Date().getDate() + 1), //plus one day
     },
     jsonSecret
-  )
-}
-
-
-
+  );
+};
 
 exports.createUser = async (req, res, next) => {
   try {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const error = new Error('Validation failed.')
-      error.statusCode = 422
-      error.data = errors.array()
-      throw error
+      const error = new Error("Validation failed.");
+      error.statusCode = 422;
+      error.data = errors.array();
+      throw error;
     }
 
-    const stageName = req.body.stageName
-    const email = req.body.email
-    const password = req.body.password
+    const stageName = req.body.stageName;
+    const email = req.body.email;
+    const password = req.body.password;
 
-    const hashedPw = await bcrypt.hash(password, 12)
+    const hashedPw = await bcrypt.hash(password, 12);
     const user = new User({
       email: email.toLowerCase(),
       stageName: stageName,
       password: hashedPw,
-    })
-    await user.save()
+    });
+    await user.save();
 
-    res.status(201).json({ message: 'User created' })
+    res.status(201).json({ message: "User created" });
 
     // transporter.sendMail({
     //   to: email,
@@ -73,31 +70,29 @@ exports.createUser = async (req, res, next) => {
     // })
   } catch (err) {
     if (!err.statusCode) {
-      err.statusCode = 500
+      err.statusCode = 500;
     }
-    next(err)
+    next(err);
   }
-}
-
-
+};
 
 exports.login = async (req, res) => {
-  const token = (req.user)
+  const token = signToken(req.user);
 
   //Getting user and progress (with media) from DB
   const user = await User.findById(req.user._id)
     .populate([
       {
-        path: 'polemoves',
-        select: 'moveRef mastered notes photos',
+        path: "polemoves",
+        select: "moveRef mastered notes photos",
         populate: {
-          path: 'media',
-          select: 'moveRef date _id extension',
+          path: "media",
+          select: "moveRef date _id extension",
         },
       },
-      { path: 'activity.polemoveId', select: 'name' },
+      { path: "activity.polemoveId", select: "name" },
     ])
-    .exec()
+    .exec();
 
   const {
     profilePic,
@@ -108,25 +103,25 @@ exports.login = async (req, res) => {
     __v,
     polemoves,
     ...responseUser
-  } = user.toObject()
+  } = user.toObject();
 
-  console.log(responseUser)
+  console.log(responseUser);
 
   if (user.profilePic) {
-    responseUser.photoURL = `https://polepath.b-cdn.net/users/${responseUser._id}/profilepics/${user.profilePic}`
+    responseUser.photoURL = `https://polepath.b-cdn.net/users/${responseUser._id}/profilepics/${user.profilePic}`;
   }
 
   res.status(200).json({
-    message: 'Login Successful',
+    message: "Login Successful",
     token: token,
     user: responseUser,
     progress: user.polemoves,
-  })
-}
+  });
+};
 
 exports.logout = (req, res) => {
   //handle with passport
   res.status(201).json({
-    message: 'Logged out',
-  })
-}
+    message: "Logged out",
+  });
+};
--- controllers/polemoves.js
+++ controllers/polemoves.js
@@ -1,44 +1,44 @@
-const Polemove = require('../models/polemoves')
-const ObjectId = require('mongoose').Types.ObjectId
-const User = require('../models/users')
-const Media = require('../models/media')
+const Polemove = require("../models/polemoves");
+const ObjectId = require("mongoose").Types.ObjectId;
+const User = require("../models/users");
+const Media = require("../models/media");
 
-const bunnies = require('../util/bunny')
-const moment = require('moment')
+const bunnies = require("../util/bunny");
+const moment = require("moment");
 
 exports.create = async (req, res, next) => {
   try {
-    console.log(req.body)
-    const name = req.body.name
-    let otherNames = req.body.otherNames
-    const level = req.body.level
-    const mainPhoto = req.file
-    const description = req.body.description
-    let extension = null
+    console.log(req.body);
+    const name = req.body.name;
+    let otherNames = req.body.otherNames;
+    const level = req.body.level;
+    const mainPhoto = req.file;
+    const description = req.body.description;
+    let extension = null;
 
     if (mainPhoto) {
-      extension = mainPhoto.mimetype.split('/').pop()
+      extension = mainPhoto.mimetype.split("/").pop();
     }
 
-    if (otherNames !== 'undefined') {
-      if (otherNames.includes(',')) {
-        otherNames = otherNames.split(',')
+    if (otherNames !== "undefined") {
+      if (otherNames.includes(",")) {
+        otherNames = otherNames.split(",");
       } else {
-        otherNames[0] = otherNames
+        otherNames[0] = otherNames;
       }
     } else {
-      otherNames = []
+      otherNames = [];
     }
 
     const newMove = new Polemove({
       name: name,
       otherNames: otherNames,
       level: level,
-      description: description === 'undefined' ? '' : description,
+      description: description === "undefined" ? "" : description,
       extension: extension,
-    })
+    });
 
-    const createdMove = await newMove.save()
+    const createdMove = await newMove.save();
 
     //Bunny photo upload
     if (extension) {
@@ -46,35 +46,37 @@ exports.create = async (req, res, next) => {
         fsFileName: mainPhoto.filename,
         bunnyFileName: `${createdMove._id}.${extension}`,
         path: `mainphotos`,
-      }
+      };
 
-      await bunnies.upload(bunnyData)
+      await bunnies.upload(bunnyData);
     }
 
-    const { __v, createdAt, updatedAt, ...responseMove } = newMove.toObject()
-    res.status(201).json({ message: 'Polemove created', newMove: responseMove })
+    const { __v, createdAt, updatedAt, ...responseMove } = newMove.toObject();
+    res
+      .status(201)
+      .json({ message: "Polemove created", newMove: responseMove });
   } catch (err) {
     if (!err.statusCode) {
-      err.statusCode = 500
+      err.statusCode = 500;
     }
-    next(err)
+    next(err);
   }
-}
+};
 
 exports.viewAll = async (req, res, next) => {
   try {
     //get all moves
     const polemoves = await Polemove.find({}).select(
-      'name level description otherNames extension'
-    )
-
+      "name level description otherNames extension"
+    );
+    console.log("CHECK");
     res
       .status(200)
-      .json({ message: 'Moves fetched succesfully', polemoves: polemoves })
+      .json({ message: "Moves fetched succesfully", polemoves: polemoves });
   } catch (err) {
     if (!err.statusCode) {
-      err.statusCode = 500
+      err.statusCode = 500;
     }
-    next(err)
+    next(err);
   }
-}
+};
